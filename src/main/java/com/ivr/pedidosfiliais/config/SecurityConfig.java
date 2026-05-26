401c0cec0eb60ab650ddf065e7040c25890cc747
Mateus Souza <mateussouzaprogrammer@gmail.com>
2026-05-26 09:48:34 -0400

    Ajuste de seguranca

--- src/main/java/com/ivr/pedidosfiliais/config/SecurityConfig.java
+++ src/main/java/com/ivr/pedidosfiliais/config/SecurityConfig.java
@@ -50,7 +50,6 @@ public class SecurityConfig {
                         .requestMatchers("/styles/**", "/script/**", "/img/**").permitAll()
 
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore((Filter) securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
